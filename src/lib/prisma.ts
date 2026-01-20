ed419c82f44ab63d96f2f3d87963318d307d664a
Rodrigo Tadei <rodrigotadei104@gmail.com>
2026-01-20 00:00:01 -0300

    fix(prisma): revert to standard connection to resolve Vercel init error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,11 +3,7 @@ import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const prismaClientSingleton = () => {
-  const connectionString = process.env.DATABASE_URL
-  const pool = new Pool({ connectionString })
-  const adapter = new PrismaPg(pool)
-
-  return new PrismaClient({ adapter })
+  return new PrismaClient()
 }
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
